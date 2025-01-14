1a18942e2dffdadb9c65593f2ad1633a90541eac
Sheyk <979007117@qq.com>
2025-01-14 09:09:26 +0800

    更新

--- main.py
+++ main.py
@@ -13,256 +13,107 @@ import os
 
 def get_color():
     # 获取随机颜色
-    get_colors = lambda n: list(map(lambda i: "#" + "%06x" % random.randint(0, 0xFFFFFF), range(n)))
-    color_list = get_colors(100)
-    return random.choice(color_list)
+    return f"#{random.randint(0, 0xFFFFFF):06x}"
 
 
 def get_access_token():
-    # appId
     app_id = config["app_id"]
-    # appSecret
     app_secret = config["app_secret"]
-    post_url = ("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={}&secret={}"
-                .format(app_id, app_secret))
+    post_url = f"https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={app_id}&secret={app_secret}"
     try:
-        access_token = get(post_url).json()['access_token']
+        return get(post_url).json()['access_token']
     except KeyError:
         print("获取access_token失败，请检查app_id和app_secret是否正确")
         os.system("pause")
         sys.exit(1)
-    # print(access_token)
-    return access_token
 
 
 def get_weather(province, city):
-    # 城市id
     try:
         city_id = cityinfo.cityInfo[province][city]["AREAID"]
     except KeyError:
         print("推送消息失败，请检查省份或城市是否正确")
         os.system("pause")
         sys.exit(1)
-    # city_id = 101280101
-    # 毫秒级时间戳
-    t = (int(round(time() * 1000)))
+
     headers = {
-        "Referer": "http://www.weather.com.cn/weather1d/{}.shtml".format(city_id),
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
+        "Referer": f"http://www.weather.com.cn/weather1d/{city_id}.shtml",
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
     }
-    url = "http://d1.weather.com.cn/dingzhi/{}.html?_={}".format(city_id, t)
+    url = f"http://d1.weather.com.cn/dingzhi/{city_id}.html?_={int(round(time() * 1000))}"
     response = get(url, headers=headers)
     response.encoding = "utf-8"
-    response_data = response.text.split(";")[0].split("=")[-1]
-    response_json = eval(response_data)
-    # print(response_json)
-    weatherinfo = response_json["weatherinfo"]
-    # 天气
-    weather = weatherinfo["weather"]
-    # 最高气温
-    temp = weatherinfo["temp"]
-    # 最低气温
-    tempn = weatherinfo["tempn"]
-    # 风向
-    wd = weatherinfo["wd"]
-    # 风力
-    ws = weatherinfo["ws"]
-    return weather, temp, tempn, wd, ws
+    weatherinfo = eval(response.text.split(";")[0].split("=")[-1])["weatherinfo"]
+    return weatherinfo["weather"], weatherinfo["temp"], weatherinfo["tempn"], weatherinfo["wd"], weatherinfo["ws"]
 
 
 def get_birthday(birthday, year, today):
-    birthday_year = birthday.split("-")[0]
-    # 判断是否为农历生日
-    if birthday_year[0] == "r":
-        r_mouth = int(birthday.split("-")[1])
-        r_day = int(birthday.split("-")[2])
-        # 今年生日
-        birthday = ZhDate(year, r_mouth, r_day).to_datetime().date()
-        year_date = birthday
+    is_lunar = birthday.startswith("r")
+    r_month, r_day = (int(birthday.split("-")[1]), int(birthday.split("-")[2])) if is_lunar else (0, 0)
+    birthday_date = ZhDate(year, r_month, r_day).to_datetime().date() if is_lunar else date(year, int(birthday.split("-")[1]), int(birthday.split("-")[2]))
 
+    if today > birthday_date:
+        birthday_date = ZhDate(year + 1, r_month, r_day).to_datetime().date() if is_lunar else date(year + 1, int(birthday.split("-")[1]), int(birthday.split("-")[2]))
 
-    else:
-        # 获取国历生日的今年对应月和日
-        birthday_month = int(birthday.split("-")[1])
-        birthday_day = int(birthday.split("-")[2])
-        # 今年生日
-        year_date = date(year, birthday_month, birthday_day)
-    # 计算生日年份，如果还没过，按当年减，如果过了需要+1
-    if today > year_date:
-        if birthday_year[0] == "r":
-            # 获取农历明年生日的月和日
-            r_last_birthday = ZhDate((year + 1), r_mouth, r_day).to_datetime().date()
-            birth_date = date((year + 1), r_last_birthday.month, r_last_birthday.day)
-        else:
-            birth_date = date((year + 1), birthday_month, birthday_day)
-        birth_day = str(birth_date.__sub__(today)).split(" ")[0]
-    elif today == year_date:
-        birth_day = 0
-    else:
-        birth_date = year_date
-        birth_day = str(birth_date.__sub__(today)).split(" ")[0]
-    return birth_day
+    return (birthday_date - today).days
 
 
 def get_ciba():
     url = "https://open.iciba.com/dsapi/"
-    headers = {
-        'Content-Type': 'application/json',
-        'User-Agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
-    }
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers={'Content-Type': 'application/json', 'User-Agent': "Mozilla/5.0"})
     data = response.json()
-
-    note_en = data["content"]
-    note_ch = data["note"]
-
-    if len(note_en) > 20:
-        note_en2 = note_en[20:]
-        note_en = note_en[:20]
-    else:
-        note_en2 = ""
-
-    if len(note_ch) > 20:
-        note_ch2 = note_ch[20:]
-        note_ch = note_ch[:20]
-    else:
-        note_ch2 = ""
-
-    return note_ch, note_ch2, note_en, note_en2
+    note_en = data["content"][:20]
+    note_ch = data["note"][:20]
+    return note_ch, note_ch[20:], note_en, note_en[20:]
 
 
 def send_message(to_user, access_token, city_name, weather, max_temperature, min_temperature, note_ch, note_ch2,
-                 note_en, note_en2, love, loveT,loveTT,loveTTT,loveTTTT, wd, ws,one,days,goodMonring):
-    url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
+                 note_en, note_en2, love, loveT, loveTT, loveTTT, loveTTTT, wd, ws, one, days, goodMonring):
+    url = f"https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={access_token}"
     week_list = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
-    year = localtime().tm_year
-    month = localtime().tm_mon
-    day = localtime().tm_mday
-    today = datetime.date(datetime(year=year, month=month, day=day))
+    today = datetime.now()
     week = week_list[today.isoweekday() % 7]
-    # 获取在一起的日子的日期格式
-    love_year = int(config["love_date"].split("-")[0])
-    love_month = int(config["love_date"].split("-")[1])
-    love_day = int(config["love_date"].split("-")[2])
-    love_date = date(love_year, love_month, love_day)
-    # 获取在一起的日期差
-    love_days = str(today.__sub__(love_date)).split(" ")[0]
-    # 获取所有生日数据
-    birthdays = {}
-    for k, v in config.items():
-        if k[0:5] == "birth":
-            birthdays[k] = v
+    love_date = date(*map(int, config["love_date"].split("-")))
+    love_days = (today.date() - love_date).days
+
+    birthdays = {k: v for k, v in config.items() if k.startswith("birth")}
     data = {
         "touser": to_user,
         "template_id": config["template_id"],
         "url": "http://weixin.qq.com/download",
         "topcolor": "#FF0000",
         "data": {
-            "date": {
-                "value": "{} {}".format(today, week),
-                "color": get_color()
-            },
-            "city": {
-                "value": city_name,
-                "color": get_color()
-            },
-            "weather": {
-                "value": weather,
-                "color": get_color()
-            },
-            "min_temperature": {
-                "value": min_temperature,
-                "color": get_color()
-            },
-            "max_temperature": {
-                "value": max_temperature,
-                "color": get_color()
-            },
-            "love_day": {
-                "value": love_days,
-                "color": get_color()
-            },
-            "note_en": {
-                "value": note_en,
-                "color": get_color()
-            },
-            "note_en2": {
-                "value": note_en2,
-                "color": get_color()
-            },
-            "note_ch": {
-                "value": note_ch,
-                "color": get_color()
-            },
-            "note_ch2": {
-                "value": note_ch2,
-                "color": get_color()
-            },
-            "love": {
-                "value": love,
-                "color": get_color()
-            },
-            "loveT": {
-                "value": loveT,
-                "color": get_color()
-            },
-            "loveTT": {
-                "value": loveTT,
-                "color": get_color()
-            },
-            "loveTTT": {
-                "value": loveTTT,
-                "color": get_color()
-            },
-            "loveTTTT": {
-                "value": loveTTTT,
-                "color": get_color()
-            },
-            "wd": {
-                "value": wd,
-                "color": get_color()
-            },
-            "ws": {
-                "value": ws,
-                "color": get_color()
-            },
-            "day": {
-                "value": days,
-                "color": get_color()
-            },
-            "one": {
-                "value": one,
-                "color": get_color()
-            },
-            "goodMonring": {
-                "value": goodMonring,
-                "color": get_color()
-            },
+            "date": {"value": f"{today.date()} {week}", "color": get_color()},
+            "city": {"value": city_name, "color": get_color()},
+            "weather": {"value": weather, "color": get_color()},
+            "min_temperature": {"value": min_temperature, "color": get_color()},
+            "max_temperature": {"value": max_temperature, "color": get_color()},
+            "love_day": {"value": love_days, "color": get_color()},
+            "note_en": {"value": note_en, "color": get_color()},
+            "note_en2": {"value": note_en2, "color": get_color()},
+            "note_ch": {"value": note_ch, "color": get_color()},
+            "note_ch2": {"value": note_ch2, "color": get_color()},
+            "love": {"value": love, "color": get_color()},
+            "loveT": {"value": loveT, "color": get_color()},
+            "loveTT": {"value": loveTT, "color": get_color()},
+            "loveTTT": {"value": loveTTT, "color": get_color()},
+            "loveTTTT": {"value": loveTTTT, "color": get_color()},
+            "wd": {"value": wd, "color": get_color()},
+            "ws": {"value": ws, "color": get_color()},
+            "day": {"value": days, "color": get_color()},
+            "one": {"value": one, "color": get_color()},
+            "goodMonring": {"value": goodMonring, "color": get_color()},
         }
     }
 
-    for key, value in birthdays.items():
-        # 获取距离下次生日的时间
-        birth_day = get_birthday(value["birthday"], year, today)
-        if birth_day == 0:
-            birthday_data = "今天{}生日哦，祝{}生日快乐！".format(value["name"], value["name"])
-        else:
-            birthday_data = "距离{}的生日还有{}天".format(value["name"], birth_day)
-        # 将生日数据插入data
-        data["data"][key] = {"value": birthday_data, "color": get_color()}
-    headers = {
-        'Content-Type': 'application/json',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-                      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
-    }
-    response = post(url, headers=headers, json=data).json()
-    if response["errcode"] == 40037:
-        print("推送消息失败，请检查模板id是否正确")
-    elif response["errcode"] == 40036:
-        print("推送消息失败，请检查模板id是否为空")
-    elif response["errcode"] == 40003:
-        print("推送消息失败，请检查微信号是否正确")
+    for k, v in birthdays.items():
+        birth_day = get_birthday(v["birthday"], today.year, today.date())
+        birthday_data = f"今天{v['name']}生日哦，祝{v['name']}生日快乐！" if birth_day == 0 else f"距离{v['name']}的生日还有{birth_day}天"
+        data["data"][k] = {"value": birthday_data, "color": get_color()}
+
+    response = post(url, headers={'Content-Type': 'application/json', 'User-Agent': 'Mozilla/5.0'}, json=data).json()
+    if response["errcode"] in {40037, 40036, 40003}:
+        print("推送消息失败，请检查模板id或微信号是否正确")
     elif response["errcode"] == 0:
         print("推送消息成功")
     else:
@@ -270,104 +121,37 @@ def send_message(to_user, access_token, city_name, weather, max_temperature, min
 
 
 def get_random_data_from_json_files():
-    directory = '.'  # 当前目录
-
-    # 获取目录下所有json文件
-    files = [f for f in os.listdir(directory) if f.endswith('.json')]
-
+    files = [f for f in os.listdir('.') if f.endswith('.json')]
     if not files:
         return "No json files found in the directory."
 
-    # 随机选择一个json文件
-    file = random.choice(files)
-
-    with open(os.path.join(directory, file), 'r', encoding='utf-8') as f:
+    with open(random.choice(files), 'r', encoding='utf-8') as f:
         data = json.load(f)
 
-    data_length = len(data)
-
-    # 生成随机索引
-    random_index = random.randint(1, data_length)
-    # 获取随机数据
-    random_data = data[str(random_index)]
-
-    return random_data + "  to 周舒萍"
+    random_index = random.randint(1, len(data))
+    return data[str(random_index)] + "  to 周舒萍"
 
 
 def getDay():
-    start_date = datetime(2024, 10, 3)
+    return (datetime.now() - datetime(2024, 10, 3)).days
 
-    # 获取当前日期作为结束日期
-    end_date = datetime.now()
-
-    # 计算日期间隔
-    days_diff = (end_date - start_date).days
-
-    return days_diff
+def getOneDay():
+    return (datetime.now() - datetime(2024, 11, 23)).days
 
 def getLenLove(loves):
-    strLen = len(loves)
-    print("strLen", strLen)
-    if strLen<=20:
-        love = loves[:20]
-        loveT = ''
-        loveTT =''
-        loveTTT =''
-        loveTTTT = ""
-        return love,loveT,loveTT,loveTTT,loveTTTT
-    elif 20 < strLen <= 40:
-        love = loves[:20]
-        loveT = loves[20:40]
-        loveTT = ''
-        loveTTT = ''
-        loveTTTT = ""
-        return love,loveT,loveTT,loveTTT,loveTTTT
-    elif 40 < strLen <= 60:
-        love = loves[:20]
-        loveT = loves[20:40]
-        loveTT = loves[40:60]
-        loveTTT = ''
-        loveTTTT = ""
-        return love,loveT,loveTT,loveTTT,loveTTTT
-    elif 60 < strLen <= 80:
-        love = loves[:20]
-        loveT = loves[20:40]
-        loveTT = loves[40:60]
-        loveTTT = loves[60:80]
-        loveTTTT = ""
-        return love,loveT,loveTT,loveTTT,loveTTTT
-    elif 80 < strLen <= 100:
-        love = loves[:20]
-        loveT = loves[20:40]
-        loveTT = loves[40:60]
-        loveTTT = loves[60:80]
-        loveTTTT = loves[80:100]
-        return love, loveT, loveTT, loveTTT,loveTTTT
-    else:
-        love = "我喜欢你。"
-        loveT = ''
-        loveTT =''
-        loveTTT =''
-        loveTTTT = ""
-        return love, loveT, loveTT, loveTTT,loveTTTT
+    love_parts = [loves[i:i + 20] for i in range(0, min(len(loves), 100), 20)]
+    return (*love_parts, *[""] * (5 - len(love_parts)))  # Fill with empty strings if less than 5 parts
+
 
 if __name__ == "__main__":
-
     try:
-        # 读取配置文件
         with open("config.txt", encoding="utf-8") as f:
             config = eval(f.read())
-    except FileNotFoundError:
-        print("推送消息失败，请检查config.txt文件是否与程序位于同一路径")
-        os.system("pause")
-        sys.exit(1)
-    except SyntaxError:
-        print("推送消息失败，请检查配置文件格式是否正确")
+    except (FileNotFoundError, SyntaxError) as e:
+        print(f"推送消息失败，请检查config.txt文件是否与程序位于同一路径: {e}")
         os.system("pause")
         sys.exit(1)
 
-    # 其余代码继续执行公众号消息推送操作
-    # 获取accessToken
     accessToken = get_access_token()
     users = config["user"]
     province, city = config["province"], config["city"]
@@ -376,12 +160,9 @@ if __name__ == "__main__":
     loves = get_random_data_from_json_files()
 
     love, loveT, loveTT, loveTTT, loveTTTT = getLenLove(loves)
-    print("love:" + love, "loveT:" + loveT, "loveTT:" + loveTT, "loveTTT:" + loveTTT, "loveTTTT:" + loveTTTT)
-
-    one = ""
+    one = getOneDay()
     day = getDay()
     goodMonring = "早安，希望你今天开开心心。"
-    print(day)
     for user in users:
         send_message(user, accessToken, city, weather, max_temperature, min_temperature, note_ch, note_ch2, note_en,
                      note_en2, love, loveT, loveTT, loveTTT, loveTTTT, wd, ws, one, day, goodMonring)
